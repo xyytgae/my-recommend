a644447fbb53f8a2a2105d184cad299b866ddf0d
xyytgae <xyytgae@yahoo.co.jp>
2022-10-07 01:17:09 +0900

    feat: #3 仮のrecommendのfetch処理実装

---
+++ apis/recommend.ts
@@ -0,0 +1,54 @@
+import { Recommend } from 'types/index'
+
+const RECOMMENDS = [
+  {
+    id: 1,
+    title: 'タイトル1',
+    userId: 1,
+    likes: [1],
+    detail: '詳細詳細',
+    images: [
+      '/images/pic1.jpg',
+      '/images/pic2.jpg',
+      '/images/pic3.jpg',
+      '/images/pic4.jpg',
+    ],
+  },
+  {
+    id: 2,
+    title: 'タイトル2タイトル2タイトル2タイトル2タイトル2タイトル2',
+    userId: 1,
+    likes: [1, 2],
+    detail:
+      '詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2',
+    images: ['/images/pic5.jpg', '/images/pic6.jpg'],
+  },
+  {
+    id: 3,
+    title: 'タイトル3タイトル3タイトル3タイトル3タイトル3',
+    userId: 1,
+    likes: [1, 2],
+    detail: '詳細3詳細3',
+    images: ['/images/pic7.jpg'],
+  },
+  {
+    id: 4,
+    title: 'タイトル4',
+    userId: 1,
+    likes: [1, 2, 3, 4],
+    detail: '詳細4詳細4',
+    images: [],
+  },
+]
+
+export const useRecommend = () => {
+  return {
+    getRecommends(): Recommend[] {
+      return RECOMMENDS
+    },
+    getRecommend(id: number): Recommend | undefined {
+      const found = RECOMMENDS.find((recommend) => recommend.id === id)
+      return found
+    },
+  }
+}
