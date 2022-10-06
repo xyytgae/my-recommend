ae2df9ca4e3ec8475df5cefa0cf4e0cfb63f5012
xyytgae <xyytgae@yahoo.co.jp>
2022-10-07 01:19:09 +0900

    chore: #3 eslintrc修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,31 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
   extends: [
     // '@nuxtjs/eslint-config-typescript',
     // 'plugin:nuxt/recommended',
     // 'prettier',
     'plugin:vue/vue3-recommended',
   ],
-  plugins: [],
+  plugins: ['vue', '@typescript-eslint'],
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': 'off',
+    // 'vue/max-attributes-per-line': [
+    //   'error',
+    //   {
+    //     singleline: 5,
+    //     multiline: {
+    //       max: 1,
+    //       allowFirstLine: false,
+    //     },
+    //   },
+    // ],
   },
 }
