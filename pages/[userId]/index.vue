721acfa13751a407f0a3206c652bd1ba9d18ec79
xyytgae <xyytgae@yahoo.co.jp>
2022-10-26 21:25:35 +0900

    chore: #13 Nuxt 3にアップグレードによるvueファイルの変更

---
+++ pages/[userId]/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>ユーザーページ</h1>
+  </div>
+</template>
--- pages/[userId]/status/[statusId].vue
+++ pages/[userId]/status/[statusId].vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  name: 'PagesUserIdStatusStatusId',
+  name: 'PagesUserIdStatusStatusId'
 }
 </script>
 
