a88afe40a6a265764d0d69fa986c9ea3ad9f1d7e
xyytgae <xyytgae@yahoo.co.jp>
2022-12-20 22:24:54 +0900

    feat: #3 recommendクエリ周り実装

--- apis/recommend.ts
+++ apis/recommend.ts
@@ -1,62 +1,42 @@
-import { Recommend } from 'types/index'
+import { gql, useQuery } from '@urql/vue'
+import { Query, QueryRecommendsCollectionArgs } from '~/src/gql/graphql'
 
-const RECOMMENDS = [
-  {
-    id: 1,
-    title: 'タイトル1',
-    userId: 1,
-    likes: [1],
-    detail: '詳細詳細',
-    images: [
-      '/images/pic1.jpg',
-      '/images/pic2.jpg',
-      '/images/pic3.jpg',
-      '/images/pic4.jpg'
-    ],
-    categoryId: 101,
-    createdAt: '2022/10/1'
-  },
-  {
-    id: 2,
-    title: 'タイトル2タイトル2タイトル2タイトル2タイトル2タイトル2',
-    userId: 1,
-    likes: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
-    detail:
-      '詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2詳細2',
-    images: ['/images/pic5.jpg', '/images/pic6.jpg'],
-    categoryId: 102,
-    createdAt: '2022/10/5'
-  },
-  {
-    id: 3,
-    title: 'タイトル3タイトル3タイトル3タイトル3タイトル3',
-    userId: 1,
-    likes: [1, 2],
-    detail: '詳細3詳細3',
-    images: ['/images/pic7.jpg'],
-    categoryId: 108,
-    createdAt: '2022/10/8'
-  },
-  {
-    id: 4,
-    title: 'タイトル4',
-    userId: 1,
-    likes: [1, 2, 3, 4],
-    detail: '詳細4詳細4',
-    images: [],
-    categoryId: 109,
-    createdAt: '2022/9/27'
-  }
-]
+export type GetRecommends = NonNullable<Query['recommendsCollection']>['edges']
 
-export const useRecommend = () => {
-  return {
-    getRecommends(): Recommend[] {
-      return RECOMMENDS
-    },
-    getRecommend(id: number): Recommend | undefined {
-      const found = RECOMMENDS.find((recommend) => recommend.id === id)
-      return found
+export const getRecommends = gql`
+  query ($orderBy: Int!) {
+    recommendsCollection(orderBy: $orderBy) {
+      edges {
+        node {
+          id
+          createdAt
+          title
+          detail
+          likesCount
+          userId
+          images
+          categoryId
+          user {
+            id
+            userName
+            avatarUrl
+          }
+        }
+      }
     }
   }
-}
+`
+export const useRecommend = () => ({
+  async getRecommends({
+    orderBy
+  }: QueryRecommendsCollectionArgs): Promise<GetRecommends> {
+    const { data } = await useQuery({
+      query: getRecommends,
+      variables: {
+        orderBy
+      }
+    })
+    console.log(data.value.recommendsCollection.edges)
+    return data.value.recommendsCollection.edges
+  }
+})
