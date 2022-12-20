ad3c3719f2e789b125a23d3164252d163872ed75
xyytgae <xyytgae@yahoo.co.jp>
2022-12-20 22:25:59 +0900

    feat: #3 pages/indexに暫定的にmiddleware実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { definePageMeta } from '#imports'
+
+definePageMeta({
+  middleware: ['auth']
+})
+</script>
 
 <template>
   <div>
