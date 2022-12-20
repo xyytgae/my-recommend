0e568b799505926a81e356d018453a8944b859c4
xyytgae <xyytgae@yahoo.co.jp>
2022-12-20 22:25:21 +0900

    feat: #3 setup用sql実装

--- sql/main.sql
+++ sql/main.sql
@@ -1,205 +1,87 @@
 -- テーブルを作成
-DROP TABLE IF EXISTS public.likes;
+DROP TABLE IF EXISTS public.likes CASCADE;
 
-DROP TABLE IF EXISTS public.recommends;
+DROP TABLE IF EXISTS public.recommends CASCADE;
 
-DROP TABLE IF EXISTS public.users;
+DROP TABLE IF EXISTS public.users CASCADE;
 
--- DROP extension IF EXISTS "uuid-ossp";
-CREATE extension IF NOT EXISTS "uuid-ossp" WITH schema extensions;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA extensions;
+
+COMMENT ON SCHEMA public IS '@graphql({"inflect_names": true})';
 
 CREATE TABLE public.users (
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  updated_at TIMESTAMP WITH time zone,
+  id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+  updated_at timestamp with time zone,
   user_name text UNIQUE,
   avatar_url text,
   twitter_url text,
   instagram_url text,
-  PRIMARY KEY(id),
-  -- FOREIGN KEY(id) REFERENCES auth.users(id),
-  CONSTRAINT user_name_length CHECK (char_length(user_name) >= 3)
+  PRIMARY KEY (id),
+  -- FOREIGN KEY(id) REFERENCES auth.public.users(id),
+  CONSTRAINT user_name_length CHECK (CHAR_LENGTH(user_name) >= 3)
 );
 
 CREATE TABLE public.recommends (
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  created_at TIMESTAMP WITH time zone DEFAULT timezone('utc' :: text, NOW()) NOT NULL,
-  title VARCHAR(50),
+  id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+  created_at timestamp with time zone DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
+  title varchar(50),
   detail text,
   images text ARRAY,
   user_id uuid,
-  -- user_id uuid NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(user_id) REFERENCES public.users (id)
+  category_id text,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES public.users (id)
 );
 
 CREATE TABLE public.likes (
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  created_at TIMESTAMP WITH time zone DEFAULT timezone('utc' :: text, NOW()) NOT NULL,
+  id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+  created_at timestamp with time zone DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
   user_id uuid NOT NULL,
   recommend_id uuid NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(user_id) REFERENCES public.users (id),
-  FOREIGN KEY(recommend_id) REFERENCES public.recommends (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES public.users (id),
+  FOREIGN KEY (recommend_id) REFERENCES public.recommends (id)
 );
 
--- RLSを有効化
-ALTER TABLE
-  public.users enable ROW LEVEL SECURITY;
-
-ALTER TABLE
-  public.recommends enable ROW LEVEL SECURITY;
-
-ALTER TABLE
-  public.likes enable ROW LEVEL SECURITY;
-
--- ポリシーを作成
-CREATE POLICY "usersを誰でもアクセスできる" ON public.users FOR
+CREATE FUNCTION public._likes_count (rec public.recommends)
+  RETURNS integer IMMUTABLE STRICT
+  LANGUAGE SQL
+  AS '
 SELECT
-  USING (TRUE);
+  count(*)
+from
+  public.likes
+where
+  recommend_id = rec.id '
+;
 
-CREATE POLICY "usersを誰でも作成可能" ON public.users FOR
-INSERT
-  WITH CHECK (TRUE);
-
-CREATE POLICY "recommendsを誰でもアクセスできる" ON public.recommends FOR
-SELECT
-  USING (TRUE);
-
-CREATE POLICY "likesを誰でもアクセスできる" ON public.likes FOR
-SELECT
-  USING (TRUE);
-
--- データを追加
-INSERT INTO
-  public.users (
-    user_name,
-    avatar_url,
-    twitter_url,
-    instagram_url
-  )
-VALUES
-  ('test_user1', '/images/pic6.jpg', '', ''),
-  ('test_user2', '/images/pic7.jpg', '', ''),
-  ('test_user3', '', '', '');
-
-INSERT INTO
-  public.recommends (created_at, title, detail, images, user_id)
-VALUES
-  (
-    '2022/11/1',
-    'タイトル1',
-    '詳細詳細1',
-    ARRAY ['/images/pic1.jpg', '/images/pic3.jpg', '/images/pic2.jpg'],
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user1'
-    )
-  ),
-  (
-    '2022/10/1',
-    'タイトル2',
-    '詳細詳細2',
-    ARRAY ['/images/pic4.jpg', '/images/pic5.jpg'],
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user2'
-    )
-  ),
-  (
-    '2022/9/1',
-    'タイトル3',
-    '詳細詳細3',
-    NULL,
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user3'
-    )
-  );
-
-INSERT INTO
-  public.likes (user_id, recommend_id)
-VALUES
-  (
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user1'
-    ),
-    (
-      SELECT
-        id
-      FROM
-        public.recommends
-      WHERE
-        title = 'タイトル1'
-    )
-  ),
-  (
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user2'
-    ),
-    (
-      SELECT
-        id
-      FROM
-        public.recommends
-      WHERE
-        title = 'タイトル1'
-    )
-  ),
-  (
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user3'
-    ),
-    (
-      SELECT
-        id
-      FROM
-        public.recommends
-      WHERE
-        title = 'タイトル1'
-    )
-  ),
-  (
-    (
-      SELECT
-        id
-      FROM
-        public.users
-      WHERE
-        user_name = 'test_user3'
-    ),
-    (
-      SELECT
-        id
-      FROM
-        public.recommends
-      WHERE
-        title = 'タイトル2'
-    )
-  );
+CREATE FUNCTION public.user (rec public.recommends)
+  RETURNS SETOF record AS $$
+BEGIN
+  RETURN query
+  SELECT
+    *
+  FROM
+    public.users
+  WHERE
+    id = rec.user_id;
+END;
+  $$
+  LANGUAGE plpgsql;
+  -- RLSを有効化
+  ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
+  ALTER TABLE public.recommends ENABLE ROW LEVEL SECURITY;
+  ALTER TABLE public.likes ENABLE ROW LEVEL SECURITY;
+  -- ポリシーを作成
+  CREATE POLICY "usersを誰でもアクセスできる" ON public.users
+    FOR SELECT
+      USING (TRUE );
+  CREATE POLICY "usersを誰でも作成可能" ON public.users
+    FOR INSERT
+      WITH CHECK (TRUE );
+  CREATE POLICY "recommendsを誰でもアクセスできる" ON public.recommends
+    FOR SELECT
+      USING (TRUE );
+  CREATE POLICY "likesを誰でもアクセスできる" ON public.likes
+    FOR SELECT
+      USING (TRUE );
