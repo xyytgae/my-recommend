724f0340e8b6da15d088254f6ac57f3070b0da2d
xyytgae <xyytgae@yahoo.co.jp>
2022-12-24 00:31:14 +0900

    feat: #15 runtimeConfig修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,6 +51,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
+      supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
       graphqlUrl: process.env.GRAPHQL_URL
     }
--- nuxt.d.ts
+++ nuxt.d.ts
@@ -12,6 +12,7 @@
 declare module '@nuxt/schema' {
   interface RuntimeConfig {
     public: {
+      supabaseUrl: string
       supabaseKey: string
       graphqlUrl: string
     }
