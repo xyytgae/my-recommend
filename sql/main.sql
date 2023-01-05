d927ac2529ebfd627bab80d06953255371b49577
xyytgae <xyytgae@yahoo.co.jp>
2023-01-06 00:39:16 +0900

    feat: #15 setup用sql修正

--- sql/main.sql
+++ sql/main.sql
@@ -5,6 +5,10 @@ DROP TABLE IF EXISTS public.recommends CASCADE;
 
 DROP TABLE IF EXISTS public.users CASCADE;
 
+DROP TABLE IF EXISTS public.images CASCADE;
+
+DROP TABLE IF EXISTS public.hashtags CASCADE;
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA extensions;
 
 COMMENT ON SCHEMA public IS '@graphql({"inflect_names": true})';
@@ -24,13 +28,30 @@ CREATE TABLE public.users (
 CREATE TABLE public.recommends (
   id uuid NOT NULL DEFAULT uuid_generate_v4 (),
   created_at timestamp with time zone DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
-  title varchar(50),
-  detail text,
-  images text ARRAY,
   user_id uuid,
   category_id text,
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES public.users (id)
+);
+
+CREATE TABLE public.images (
+  id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+  created_at timestamp with time zone DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
+  url varchar(2083) NOT NULL,
+  recommend_id uuid NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (recommend_id) REFERENCES public.recommends (id)
+);
+
+CREATE TABLE public.hashtags (
+  id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+  created_at timestamp with time zone DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
+  text text NOT NULL,
+  x varchar(8) NOT NULL DEFAULT '',
+  y varchar(8) NOT NULL DEFAULT '',
+  image_id uuid NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (image_id) REFERENCES public.images (id)
 );
 
 CREATE TABLE public.likes (
@@ -68,6 +89,32 @@ BEGIN
 END;
   $$
   LANGUAGE plpgsql;
+  CREATE FUNCTION public.image (rec public.recommends )
+    RETURNS SETOF record AS $$
+    BEGIN
+      RETURN query
+      SELECT
+        *
+      FROM
+        public.images
+      WHERE
+        recommend_id = rec.id;
+END;
+  $$
+  LANGUAGE plpgsql;
+  CREATE FUNCTION public.hashtag (rec public.images )
+    RETURNS SETOF record AS $$
+    BEGIN
+      RETURN query
+      SELECT
+        *
+      FROM
+        public.hashtags
+      WHERE
+        image_id = rec.id;
+END;
+  $$
+  LANGUAGE plpgsql;
   -- RLSを有効化
   ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
   ALTER TABLE public.recommends ENABLE ROW LEVEL SECURITY;
@@ -82,6 +129,21 @@ END;
   CREATE POLICY "recommendsを誰でもアクセスできる" ON public.recommends
     FOR SELECT
       USING (TRUE );
+  CREATE POLICY "recommendsを誰でも作成可能" ON public.recommends
+    FOR INSERT
+      WITH CHECK (TRUE );
   CREATE POLICY "likesを誰でもアクセスできる" ON public.likes
     FOR SELECT
       USING (TRUE );
+  CREATE POLICY "imagesを誰でもアクセスできる" ON public.images
+    FOR SELECT
+      USING (TRUE );
+  CREATE POLICY "imagesを誰でも作成可能" ON public.images
+    FOR INSERT
+      WITH CHECK (TRUE );
+  CREATE POLICY "hashtagsを誰でもアクセスできる" ON public.hashtags
+    FOR SELECT
+      USING (TRUE );
+  CREATE POLICY "hashtagsを誰でも作成可能" ON public.hashtags
+    FOR INSERT
+      WITH CHECK (TRUE );
