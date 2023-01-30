9896f4173b72ea29a469c6415b3a226b58c37124
xyytgae <xyytgae@yahoo.co.jp>
2023-01-31 01:04:29 +0900

    feature: #28 yarn codegen

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -3,10 +3,20 @@ import * as types from './graphql'
 import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
 
 const documents = {
+  '\n  mutation CreateLike($objects: [LikesInsertInput!]!) {\n    insertIntoLikesCollection(objects: $objects) {\n      affectedCount\n    }\n  }\n':
+    types.CreateLikeDocument,
+  '\n  mutation DeleteLike($filter: LikesFilter!) {\n    deleteFromLikesCollection(filter: $filter) {\n      affectedCount\n    }\n  }\n':
+    types.DeleteLikeDocument,
   '\n  query GetUser($filter: UsersFilter!, $first: Int!) {\n    usersCollection(filter: $filter, first: $first) {\n      edges {\n        node {\n          id\n          avatarUrl\n          name\n          email\n        }\n      }\n    }\n  }\n':
     types.GetUserDocument
 }
 
+export function gql(
+  source: '\n  mutation CreateLike($objects: [LikesInsertInput!]!) {\n    insertIntoLikesCollection(objects: $objects) {\n      affectedCount\n    }\n  }\n'
+): typeof documents['\n  mutation CreateLike($objects: [LikesInsertInput!]!) {\n    insertIntoLikesCollection(objects: $objects) {\n      affectedCount\n    }\n  }\n']
+export function gql(
+  source: '\n  mutation DeleteLike($filter: LikesFilter!) {\n    deleteFromLikesCollection(filter: $filter) {\n      affectedCount\n    }\n  }\n'
+): typeof documents['\n  mutation DeleteLike($filter: LikesFilter!) {\n    deleteFromLikesCollection(filter: $filter) {\n      affectedCount\n    }\n  }\n']
 export function gql(
   source: '\n  query GetUser($filter: UsersFilter!, $first: Int!) {\n    usersCollection(filter: $filter, first: $first) {\n      edges {\n        node {\n          id\n          avatarUrl\n          name\n          email\n        }\n      }\n    }\n  }\n'
 ): typeof documents['\n  query GetUser($filter: UsersFilter!, $first: Int!) {\n    usersCollection(filter: $filter, first: $first) {\n      edges {\n        node {\n          id\n          avatarUrl\n          name\n          email\n        }\n      }\n    }\n  }\n']
